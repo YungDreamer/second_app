ba1e125593859d6673ee4e62f0acababd0a773f3
yungdreamer@yandex.ru <yungdreamer@yandex.ru>
2018-12-04 15:56:52 +0300

    Create and add id

--- src/components/ArrDisplay/index.js
+++ src/components/ArrDisplay/index.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
 
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import Redirect from '../Redirect/';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
 
 import './ArrDIsplay.css';
 
 export default class ArrDisplay extends Component {
     state = {
-        hits: [],
-        images: []
+        hits: []
     };
 
     makeId = () => {
@@ -17,48 +15,55 @@ export default class ArrDisplay extends Component {
 
         for (let i = 0; i < 5; i++)
             text += possible.charAt(Math.floor(Math.random() * possible.length));
-        console.log(text);
         return text;
     };
 
+    addId = ({ hits }) => {
+        return hits.map((site) => {
+            return {
+            site: site,
+            ids: this.makeId()
+            }
+        })
+    };
+
+    redirect = (q) => {
+        console.log(q);
+        //setTimeout(() => {window.open(q)}, 800);
+    }
+    
     componentWillMount() {
         this.setState({
-            hits: [{
-                site: 'ggkttd.by'
-            }, {
-                site: 'komotoz.ru/kartinki'
-            }],
-            images: [{
-                image: 'komotoz.ru/kartinki/images/prikolnie_kartinki_s_kotami/prikolnie_kartinki_s_kotami_04.jpg'
-            }, {
-                image: 'komotoz.ru/kartinki/images/prikolnie_kartinki_s_nadpisjami/prikolnie_kartinki_s_nadpisjami_01.jpg'
-            }]
+            hits: [
+                'ggkttd.by',            
+                'komotoz.ru/kartinki',           
+                'komotoz.ru/kartinki/images/prikolnie_kartinki_s_kotami/prikolnie_kartinki_s_kotami_04.jpg',          
+                'komotoz.ru/kartinki/images/prikolnie_kartinki_s_nadpisjami/prikolnie_kartinki_s_nadpisjami_01.jpg'
+            ]
         })
     }
+
     render() {
-        const { hits, images } = this.state;
+        let { hits } = this.state;
+        console.log(hits);
+         
+        hits = this.addId({hits}); 
+        console.log(hits);
+
         return (
             <Router>
                 <div className={'root'}>
                     <div className={'sidebar'}>
                     {hits.map(hit =>
                         <div className={'sidebarItem'}>
-                            <Link to={`/sample/${hit.site}`} onClick={this.timeout}>
-                                <span>{hit.site}</span>
+                            <Link to={`/${hit.ids}`} className={'test'} onClick={this.redirect(hit.site)} id={hit.ids}>
+                                <span>{hit.ids}</span>
                             </Link>
                         </div>
-                    )}
-                    {images.map(img =>
-                        <div className={'sidebarItem'}>
-                            <Link to={`/sample/${img.image}`} onClick={this.timeout}>
-                                <span>{img.image}</span>
-                            </Link>
-                        </div>
-                    )}
+                    )}                 
                     </div>
                     <div className={'main'}>
                         <h1>Welcome</h1>
-                        <Route path="/sample/:link" component={Redirect}/>
                     </div>
                 </div>
             </Router>
